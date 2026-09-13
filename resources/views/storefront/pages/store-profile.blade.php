56d90c4b21ef0a7977f2861b499c5ef8dd4563ed
arakishore <arakishore@gmail.com>
2026-09-13 13:53:12 +0530

    simple chnage: but need improvement

--- resources/views/storefront/pages/store-profile.blade.php
+++ resources/views/storefront/pages/store-profile.blade.php
@@ -72,15 +72,14 @@
                                 </a>
                             @endif --}}
                         </div>
-
-                        {{-- <div class="shop-profile-facts">
+                         <div class="shop-profile-facts">
                             <span class="shop-profile-fact"><i class="icon icon-Tag"></i>{{ $shopProfile['shop_type'] ?: 'General Store' }}</span>
                             @if (!empty($shopProfile['audiences']))
                                 <span class="shop-profile-fact"><i class="icon icon-Users"></i>{{ implode(', ', $shopProfile['audiences']) }}</span>
                             @endif
                             <span class="shop-profile-fact"><i class="icon icon-Package"></i>{{ $shopProfile['product_count'] }} products</span>
                             <span class="shop-profile-fact"><i class="icon icon-ShieldCheck"></i>Active Shop</span>
-                        </div> --}}
+                        </div>
                     </div>
 
                     <div class="shop-profile-slider">
@@ -125,12 +124,19 @@
 
             <section class="shop-profile-meta-band" aria-label="Shop details">
                 <div class="shop-profile-meta-item">
-                    <div class="shop-profile-meta-label">Shop Type</div>
-                    <div class="shop-profile-meta-value">{{ $shopProfile['shop_type'] ?: 'General Store' }}</div>
+                    <a href="##" class="link sold-by-card__whatsapp" target="_blank" rel="noopener noreferrer" aria-label="Chat with ## on WhatsApp">
+                                            <svg aria-hidden="true" class="sold-by-card__whatsapp-icon" viewBox="0 0 32 32" focusable="false">
+                                                <path d="M19.11 17.54c-.29-.14-1.71-.84-1.98-.94-.27-.1-.46-.14-.65.14-.19.29-.75.94-.92 1.13-.17.19-.34.21-.63.07-.29-.14-1.22-.45-2.33-1.44-.86-.77-1.44-1.72-1.61-2.01-.17-.29-.02-.44.13-.59.13-.13.29-.34.43-.51.14-.17.19-.29.29-.48.1-.19.05-.36-.02-.51-.07-.14-.65-1.57-.89-2.15-.23-.56-.47-.48-.65-.49h-.56c-.19 0-.51.07-.77.36-.27.29-1.01.99-1.01 2.41s1.04 2.8 1.18 2.99c.14.19 2.04 3.12 4.95 4.37.69.3 1.23.48 1.65.61.69.22 1.32.19 1.82.12.56-.08 1.71-.7 1.95-1.37.24-.67.24-1.25.17-1.37-.07-.12-.26-.19-.55-.34ZM16.03 4.8c-6.17 0-11.18 5.01-11.18 11.18 0 2.11.59 4.08 1.61 5.76L4.75 28l6.4-1.68a11.1 11.1 0 0 0 4.88 1.13c6.17 0 11.18-5.01 11.18-11.18S22.2 4.8 16.03 4.8Zm0 20.74c-1.61 0-3.18-.41-4.58-1.18l-.33-.18-3.8 1 1.01-3.7-.22-.38a9.48 9.48 0 0 1-1.36-4.89c0-5.25 4.03-9.52 9.28-9.52s9.28 4.27 9.28 9.52-4.03 9.33-9.28 9.33Z"></path>
+                                            </svg>
+                                            WhatsApp
+                                        </a>
                 </div>
                 <div class="shop-profile-meta-item">
-                    <div class="shop-profile-meta-label">Audience</div>
-                    <div class="shop-profile-meta-value">{{ !empty($shopProfile['audiences']) ? implode(', ', $shopProfile['audiences']) : 'All shoppers' }}</div>
+                    <a href="#;" class="link">
+                                        <i class="icon icon-Question"></i>
+                                        Ask A Question
+                                    </a>
+
                 </div>
                 <div class="shop-profile-meta-item">
                     <div class="shop-profile-meta-label">Location</div>
@@ -145,6 +151,10 @@
                 <div class="shop-profile-meta-item">
                     <div class="shop-profile-meta-label">Products</div>
                     <div class="shop-profile-meta-value">{{ $shopProfile['product_count'] }} available</div>
+                     <a href="#;" class="link">
+                                        <i class="icon icon-ShareNetwork"></i>
+                                        Share
+                                    </a>
                 </div>
             </section>
 
